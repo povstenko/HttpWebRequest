71d83bbebb32a1f89b50617b4c1946747b113514
Vitaly Povstenko <vitaly.povstenko@gmail.com>
2019-03-02 10:56:41 +0200

    First commit

---
+++ htttpDemo/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace htttpDemo
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.tbxAdress = new System.Windows.Forms.TextBox();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tbxSplit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.lblImage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.panelHW = new System.Windows.Forms.Panel();
+            this.lblHomeWork = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbxAdress
+            // 
+            this.tbxAdress.Location = new System.Drawing.Point(12, 10);
+            this.tbxAdress.Name = "tbxAdress";
+            this.tbxAdress.Size = new System.Drawing.Size(561, 20);
+            this.tbxAdress.TabIndex = 0;
+            this.tbxAdress.Text = "http://markforest.pro/";
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(579, 8);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(75, 23);
+            this.btnRequest.TabIndex = 1;
+            this.btnRequest.Text = "Request";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            this.btnRequest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 352);
+            this.panel1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(655, 352);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(2, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(663, 378);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(655, 352);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Full Response";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(655, 352);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "UTF8";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(655, 352);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.tbxSplit);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.listBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(655, 352);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Collection";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tbxSplit
+            // 
+            this.tbxSplit.Location = new System.Drawing.Point(120, 20);
+            this.tbxSplit.Name = "tbxSplit";
+            this.tbxSplit.Size = new System.Drawing.Size(111, 20);
+            this.tbxSplit.TabIndex = 3;
+            this.tbxSplit.Text = "|";
+            this.tbxSplit.TextChanged += new System.EventHandler(this.tbxSplit_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Split string by symbol:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(320, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 342);
+            this.listBox1.TabIndex = 1;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.lblImage);
+            this.tabPage4.Controls.Add(this.pictureBox1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(655, 352);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Image from URL";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Location = new System.Drawing.Point(6, 3);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(0, 13);
+            this.lblImage.TabIndex = 1;
+            this.lblImage.Click += new System.EventHandler(this.lblImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(655, 352);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.panelHW);
+            this.tabPage5.Controls.Add(this.lblHomeWork);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(655, 352);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "All images from site (Home Work)";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // panelHW
+            // 
+            this.panelHW.AutoScroll = true;
+            this.panelHW.Location = new System.Drawing.Point(0, 0);
+            this.panelHW.Name = "panelHW";
+            this.panelHW.Size = new System.Drawing.Size(655, 352);
+            this.panelHW.TabIndex = 3;
+            // 
+            // lblHomeWork
+            // 
+            this.lblHomeWork.AutoSize = true;
+            this.lblHomeWork.Location = new System.Drawing.Point(6, 3);
+            this.lblHomeWork.Name = "lblHomeWork";
+            this.lblHomeWork.Size = new System.Drawing.Size(0, 13);
+            this.lblHomeWork.TabIndex = 2;
+            // 
+            // fMain
+            // 
+            this.AcceptButton = this.btnRequest;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 416);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnRequest);
+            this.Controls.Add(this.tbxAdress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "HTTP Web Request";
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxAdress;
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox tbxSplit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblImage;
+        private System.Windows.Forms.Label lblHomeWork;
+        private System.Windows.Forms.Panel panelHW;
+    }
+}
+
---
+++ htttpDemo/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace htttpDemo
+{
+    public partial class fMain : Form
+    {
+        HttpWebRequest req;
+        HttpWebResponse res;
+        StreamReader sr;
+
+        WebClient wc;
+
+        static string pattern = @"(https?:)?//?[^'""<>]+?\.(jpg|jpeg|gif|png)";
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            req = (HttpWebRequest)HttpWebRequest.Create(tbxAdress.Text);
+            res = (HttpWebResponse)req.GetResponse();
+            sr = new StreamReader(res.GetResponseStream(), Encoding.Default);
+            richTextBox1.Text = sr.ReadToEnd();
+            sr.Close();
+
+            wc = new WebClient();
+            byte[] urlData = wc.DownloadData(tbxAdress.Text);
+            string page = Encoding.UTF8.GetString(urlData);
+            richTextBox2.Text = page;
+
+            listBox1.Items.Clear();
+            foreach (var item in page.Split(tbxSplit.Text[0]))
+            {
+                listBox1.Items.Add(item);
+            }
+
+            try
+            {
+                //wc = new WebClient();
+                string fileCopy = "D:\\image.jpg";
+                string urlString = tbxAdress.Text;
+                wc.DownloadFile(urlString, fileCopy);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = Image.FromFile(fileCopy);
+            }
+            catch(Exception ex)
+            {
+                lblImage.Text = ex.Message;
+            }
+
+            //homework
+            LoadData();
+            try
+            {
+                wc = new WebClient();
+
+                string input = richTextBox1.Text;
+                int cntr = 1;
+
+                int offset = 0;
+                int offset_Y = 0;
+                string filename = "";
+                
+                foreach (Match m in Regex.Matches(input, pattern))
+                {
+                    filename = $"images\\image{cntr++}.{Path.GetExtension(m.Value)}";
+                    wc.DownloadFile(m.Value, filename);
+                    PictureBox pb = new PictureBox();
+                    pb.Width = 100;
+                    pb.Height = 100;
+                    pb.Location = new Point(pb.Location.X + offset, pb.Location.Y + offset_Y);
+                    offset += 150;
+                    if (offset > this.Width - 150)
+                    {
+                        offset_Y += 150;
+                        offset = 0;
+                    }
+                    pb.SizeMode = PictureBoxSizeMode.Zoom;
+                    pb.Image = Image.FromFile(filename);
+                    panelHW.Controls.Add(pb);
+                }
+            }
+            catch (Exception ex)
+            {
+                lblHomeWork.Text = ex.Message;
+            }
+            wc = null;
+
+        }
+
+        private void lblImage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxSplit_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxSplit.Text != "")
+                tbxSplit.Text = tbxSplit.Text[0].ToString();
+            else
+                tbxSplit.Text = "|";
+        }
+
+        private void LoadData()
+        {
+           
+        }
+    }
+}
